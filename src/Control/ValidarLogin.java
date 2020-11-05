864952f535b165ea4cb3d7634da4e929b5250993
juand <juand@DESKTOP-KVMSSS0>
2020-11-04 21:48:18 -0500

    LabRealizado

---
+++ src/Control/ValidarLogin.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Control;
+
+import Entidad.Usuario;
+
+/**
+ *
+ * @author juand JUAN DAVID SANTAMARIA ROJAS C:\Users\juand\Documents\NetBeansProjects\LoginApplication`
+ */
+public class ValidarLogin {
+    
+    public ValidarLogin(){
+    }
+    
+    public String verificarLogin (Usuario usuario){
+        return null;
+    }
+    
+    public boolean verificarLongitudNombre (String nombre){
+        return true;
+    }
+    
+    public boolean verificarLongitudPassword (String nombre){
+        return true;
+    }
+    
+}
